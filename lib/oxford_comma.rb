dc7b61d4cf98b2d8583c10f7ebbcd8054f13d6bb
tuanlong1202 <tuanlong1202@gmail.com>
2021-07-23 01:06:07 -0400

    Phase 3 Oxford comma

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,18 @@
 def oxford_comma(array)
-
+    retString = ""
+    size = array.size
+    array.each_with_index do |item,index|
+        if size == 1 || index == 0
+            retString = String(item)
+        elsif size == 2 && index == 1
+            retString = retString + " and " + String(item)
+        elsif size > 1
+            if index + 1 == size
+                retString = retString + ", and " + String(item) 
+            elsif index + 1 < size
+                retString = retString + ", " + String(item)
+            end
+        end
+    end
+    retString
 end
